a04c58d5e1cdee2d0f7ef8e71d9fa338224a9b32
N.N.L.Keerthana <nyshadham21bcs185@iiitkottayam.ac.in>
2023-05-31 21:49:15 +0530

    Edit 8.1

--- Flutter/mur/test/widget_test.dart
+++ Flutter/mur/test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mur/components/main.dart';
+import 'package:mur/main.dart';
 
 
 void main() {
